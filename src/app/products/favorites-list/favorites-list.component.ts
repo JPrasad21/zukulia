cee50381ee28f631d65a64586666f6b2db9c7c31
prasad_jayanarasimhan <pjayanarasimhan@concord.net>
2025-01-26 15:14:27 +0530

    Updated favorites empty page message and imaging numbers

--- src/app/products/favorites-list/favorites-list.component.ts
+++ src/app/products/favorites-list/favorites-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, computed, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-favorites-list',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, RouterModule, MatButtonModule],
   templateUrl: './favorites-list.component.html',
   styleUrl: './favorites-list.component.scss',
 })
